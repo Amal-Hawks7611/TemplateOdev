4d7766a9e246f9db9fe9eb3abef87b69cef8cc88
Kaedari <bayramkarabal083@gmail.com>
2025-12-30 22:33:50 +0300

    Update Intake.java

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -5,9 +5,9 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix6.hardware.TalonFX;
-import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class Intake extends Command { //bilmiom
+public class Intake extends SubsystemBase { //artık biliom
   public static TalonFX motor;
   public Intake() {
     motor = new TalonFX(15);//...
