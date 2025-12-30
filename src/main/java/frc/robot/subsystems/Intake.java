2e583a0b985b385b71a78562da0ad3b93f081ed2
Kaedari <bayramkarabal083@gmail.com>
2025-12-30 22:23:58 +0300

    Create Intake.java

---
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -0,0 +1,21 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.hardware.TalonFX;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Intake extends SubsystemBase {
+  public static TalonFX motor;
+  public Intake() {
+    motor = new TalonFX(15);//...
+  }
+  public void Motorcalis() {
+    motor.set(1);
+  }
+  public void Motordur() {
+    motor.set(0);
+  }
+}
